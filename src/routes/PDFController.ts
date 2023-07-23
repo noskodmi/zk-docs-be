9415cc6f3a6e7c18ff71f38331d08cbbae99ffb6
Dmitrii <dmitrynoskovcz@gmail.com>
2023-07-23 02:52:04 +0200

    added node-signpdf

--- src/routes/PDFController.ts
+++ src/routes/PDFController.ts
@@ -1,15 +1,51 @@
 import type { Request, Response, NextFunction } from 'express'
-
 import prisma from '../prisma'
 import ApiError from './../errors/ApiError'
+const signer = require('node-signpdf').default
+const fs = require('fs')
+const { plainAddPlaceholder } = require('node-signpdf/dist/helpers');
+
+const pdfSignedPath = `./signed.pdf`;
+const pdf2SignedPath = `./2signed.pdf`;
+const pdfBuffer = fs.readFileSync(`./w3dummy.pdf`);
+const pdfSignedBuffer = fs.readFileSync(`./signed.pdf`);
+const certBuffer = fs.readFileSync(`./certificate.p12`);
 
 export const PDFController = {
   firstSign: async (req: Request, res: Response, next: NextFunction) => {
-
-    res.json("first")
+    let inputBuffer = plainAddPlaceholder({
+      pdfBuffer,
+      reason: 'Signed Certificate.',
+      contactInfo: 'sign@example.com',
+      name: 'Example',
+      location: 'Paris',
+      signatureLength: certBuffer.length,
+    });
+  
+    const signedPdf = signer.sign(
+      inputBuffer,
+      certBuffer,
+    { asn1StrictParsing : true },
+    );
+    fs.writeFileSync(pdfSignedPath, signedPdf);
+    res.download(pdfSignedPath)
   },
   secondSign: async (req: Request, res: Response, next: NextFunction) => {
-
-    res.json("second")
+    let inputBuffer = plainAddPlaceholder({
+      pdfSignedBuffer,
+      reason: 'Signed Certificate.',
+      contactInfo: 'sign@example.com',
+      name: 'Example',
+      location: 'Paris',
+      signatureLength: certBuffer.length,
+    });
+  
+    const signedPdf = signer.sign(
+      inputBuffer,
+      certBuffer,
+    { asn1StrictParsing : true },
+    );
+    fs.writeFileSync(pdf2SignedPath, signedPdf);
+    res.download(pdf2SignedPath)
   },
 }
